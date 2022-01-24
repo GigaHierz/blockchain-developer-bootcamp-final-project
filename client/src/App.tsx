e5c74bc37ab2fec7e5e5d64ce2893960e003ddf7
GigaHierz <lenahierzi@hotmail.de>
2022-01-24 14:08:15 +0100

    new built

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -65,6 +65,23 @@ export default function App() {
     window.location.reload();
   });
 
+  (function (l) {
+    if (l.search[1] === "/") {
+      var decoded = l.search
+        .slice(1)
+        .split("&")
+        .map(function (s) {
+          return s.replace(/~and~/g, "&");
+        })
+        .join("?");
+      window.history.replaceState(
+        null,
+        null,
+        l.pathname.slice(0, -1) + decoded + l.hash
+      );
+    }
+  })(window.location);
+
   return (
     <ChakraProvider>
       <FlexColumn>
